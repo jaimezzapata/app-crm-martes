8c5aba3c9ee8cc80762756cbfa6428eda75a7c4f
jaimezzapata <zapataval2304@gmail.com>
2025-04-22 19:06:45 -0500

    Implementación de cierre de sesión

--- src/components/MenuLateral.jsx
+++ src/components/MenuLateral.jsx
@@ -1,17 +1,49 @@
+import { alerta } from "../helpers/funciones";
+import { useNavigate } from "react-router-dom";
+
 const MenuLateral = () => {
+  let usuario = JSON.parse(localStorage.getItem("usuario"));
+  let redireccion = useNavigate();
+
+  function cerrarSesion() {
+    localStorage.removeItem("token");
+    localStorage.removeItem("usuario");
+    alerta("Bai", "Cerrando sesión", "info");
+    redireccion("/");
+  }
+
   return (
     <aside className="aplicacion__menu-lateral">
-      <h1 className="aplicacion__menu-lateral-logo">Track <span className="aplicacion__menu-lateral-logo--resaltado">X</span></h1>
-      <h2>Usuario: Admin</h2>
-      <img className="aplicacion__menu-lateral-logo-imagen" src="/logo.jpg" alt="Logo" />
+      <h1 className="aplicacion__menu-lateral-logo">
+        Track{" "}
+        <span className="aplicacion__menu-lateral-logo--resaltado">X</span>
+      </h1>
+      <h2>Usuario: {usuario.nombre}</h2>
+      <img
+        className="aplicacion__menu-lateral-logo-imagen"
+        src="/logo.jpg"
+        alt="Logo"
+      />
       <nav className="aplicacion__menu-lateral-navegacion">
-        <a className="aplicacion__menu-lateral-navegacion-item" href="">Inicio</a>
-        <a className="aplicacion__menu-lateral-navegacion-item" href="">Gestión de envíos</a>
-        <a className="aplicacion__menu-lateral-navegacion-item" href="">Gestión de clientes</a>
-        <button type='button' className="aplicacion__menu-lateral-navegacion-item">Cerrar sesión</button>
+        <a className="aplicacion__menu-lateral-navegacion-item" href="">
+          Inicio
+        </a>
+        <a className="aplicacion__menu-lateral-navegacion-item" href="">
+          Gestión de envíos
+        </a>
+        <a className="aplicacion__menu-lateral-navegacion-item" href="">
+          Gestión de clientes
+        </a>
+        <button
+          type="button"
+          onClick={cerrarSesion}
+          className="aplicacion__menu-lateral-navegacion-item"
+        >
+          Cerrar sesión
+        </button>
       </nav>
     </aside>
-  )
-}
+  );
+};
 
-export default MenuLateral
+export default MenuLateral;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,27 +1,28 @@
-import { useState } from 'react'
-import { usuarios } from '../services/database'
-import { alerta, generarToken } from '../helpers/funciones'
-import { useNavigate } from 'react-router-dom'
-import './Login.css'
+import { useState } from "react";
+import { usuarios } from "../services/database";
+import { alerta, generarToken } from "../helpers/funciones";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 function Login() {
-  const [getUser, setUser] = useState()
-  const [getPassword, setPassword] = useState()
-  let redireccion = useNavigate()
-
-
+  const [getUser, setUser] = useState();
+  const [getPassword, setPassword] = useState();
+  let redireccion = useNavigate();
 
   function buscarUsuario() {
-    let usuario = usuarios.find((item) => getUser === item.usuario && getPassword === item.contrasena)
-    return usuario
+    let usuario = usuarios.find(
+      (item) => getUser === item.usuario && getPassword === item.contrasena
+    );
+    return usuario;
   }
   function iniciarSesion() {
     if (buscarUsuario()) {
-      let tokenAcceso = generarToken()
-      localStorage.setItem("token", tokenAcceso)
-      alerta("Bienvenido", "Acceso al sistema", "success")
-      redireccion("/home")
+      let tokenAcceso = generarToken();
+      localStorage.setItem("token", tokenAcceso);
+      localStorage.setItem("usuario", JSON.stringify(buscarUsuario()));
+      alerta("Bienvenido", "Acceso al sistema", "success");
+      redireccion("/home");
     } else {
-      alerta("Error", "Error de credenciales", "error")
+      alerta("Error", "Error de credenciales", "error");
     }
   }
   return (
@@ -30,10 +31,23 @@ function Login() {
       <form className="form">
         <div className="form_front">
           <div className="form_details">Login</div>
-          <input onChange={(e) => setUser(e.target.value)} type="text" className="input" placeholder="Username" />
-          <input onChange={(e) => setPassword(e.target.value)} type="text" className="input" placeholder="Password" />
-          <button type='button' onClick={iniciarSesion} className="btn">Login</button>
-          <span className="switch">Don't have an account?
+          <input
+            onChange={(e) => setUser(e.target.value)}
+            type="text"
+            className="input"
+            placeholder="Username"
+          />
+          <input
+            onChange={(e) => setPassword(e.target.value)}
+            type="text"
+            className="input"
+            placeholder="Password"
+          />
+          <button type="button" onClick={iniciarSesion} className="btn">
+            Login
+          </button>
+          <span className="switch">
+            Don't have an account?
             <label for="signup_toggle" className="signup_tog">
               Sign Up
             </label>
@@ -46,7 +60,8 @@ function Login() {
           <input type="text" className="input" placeholder="Password" />
           <input type="text" className="input" placeholder="Confirm Password" />
           <button className="btn">Signup</button>
-          <span className="switch">Already have an account?
+          <span className="switch">
+            Already have an account?
             <label for="signup_toggle" className="signup_tog">
               Sign In
             </label>
@@ -54,7 +69,7 @@ function Login() {
         </div>
       </form>
     </div>
-  )
+  );
 }
 
-export default Login
+export default Login;
