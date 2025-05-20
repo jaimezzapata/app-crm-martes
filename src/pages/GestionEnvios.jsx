d9307a88c82d26f2a2404577a31081b80ee92748
jaimezzapata <zapataval2304@gmail.com>
2025-05-20 18:41:28 -0500

    Se crea filtro de envios por usuario

--- src/pages/GestionEnvios.jsx
+++ src/pages/GestionEnvios.jsx
@@ -1,7 +1,22 @@
-//rfce -> Crea un componente con función regular
-//rafce -> Crea un componente con función flecha
-
+import { useEffect, useState } from "react";
+let apiEnvios = "https://back-json-server-martes.onrender.com/envios";
 const GestionEnvios = () => {
+  let usuarioLogueado = JSON.parse(localStorage.getItem("usuario"))
+  const [envios, setEnvios] = useState([]);
+  function getEnvios() {
+    fetch(apiEnvios)
+      .then((response) => response.json())
+      .then((data) => setEnvios(data));
+  }
+  useEffect(() => {
+    getEnvios();
+  }, []);
+
+  function filtrarEnviosUsuario() {
+    let filtroEnvios = envios.filter((item) => item.usuarioId == usuarioLogueado.id)
+    return filtroEnvios
+  }
+
   return (
     <div>GestionEnvios</div>
   )
